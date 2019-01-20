f1e5ad9fc002e183ae18a6ff682c54b7a3e66bcc
Chris <chrisaconquet@hotmail.com>
2019-01-20 22:17:13 +0100

    Hello
    ...

---
+++ Assets/2D Levels/ColorCodes.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+///Deze kleur codes die voorkomen in de standaart mapjes 
+/// 
+/// 
+///Make sure your color has a full alpha falue!
+/// 
+/// Grass = B48232
+/// Dirt = DC8232
+/// Water/Player = 00B4E6
+/// Hill-Upperpart = 00B4E6
+/// Hill-Underpart = 00B000
+
+
+
+
